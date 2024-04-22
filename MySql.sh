2bf4d8f7dd12a341ca5373ad0120b1fec143b0c2
mastan <mastan@gmail>
2024-04-22 09:52:23 +0530

    mysql

--- MySql.sh
+++ MySql.sh
@@ -34,5 +34,13 @@ VALIDATE $? "Enabling Mysql server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting Mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting root password Mysql server"
+mysql -h db.shaik.fun -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$LOGFILE
+
+if [ $? -ne 0 ]
+then
+	mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+	VALIDATE $? "Setting root password Mysql server"
+else
+	echo -e "root password Mysql server already setup....$Y Skipping $N"
+fi
+
